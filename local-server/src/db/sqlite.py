bfb6ae40490046854cec029cc3ccd5bfe0b53157
Aruth Kandage <aruthkandage@gmail.com>
2022-08-26 18:11:19 -0400

    basic configuration, logging, database setup

---
+++ local-server/src/db/sqlite.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+def setup_db(db_config):
+    db_path = db_config.get('sqlite-db-path', 'local_server.db')
+
+def create_user_account_table(conn):
+    pass
+
+def create_directory_table(conn):
+    pass
+
+def create_file_table(conn):
+    pass
