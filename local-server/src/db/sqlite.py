3330d1e77fb64d415a81c8fe93bc41d45df3d1d9
Aruth Kandage <aruthkandage@gmail.com>
2022-08-27 16:44:40 -0400

    create all tables with ps_ prefix

--- local-server/src/db/sqlite.py
+++ local-server/src/db/sqlite.py
@@ -19,64 +19,77 @@ def setup_db(db_config):
             pass
 
 def create_user_account_table(conn):
-    cur = conn.cursor()
-    try:
-        cur.execute(
-            '''
-            CREATE TABLE user_account (
-                username VARCHAR(50) PRIMARY KEY NOT NULL,
-                password_hash VARCHAR(32) NOT NULL
-            )
-            '''
+    logging.debug('Setting up ps_user_account table')
+    conn.execute(
+        '''
+        CREATE TABLE ps_user_account (
+            username VARCHAR(50) PRIMARY KEY NOT NULL,
+            password_hash VARCHAR(32) NOT NULL
         )
-        # Create admin user with default password.
-        cur.execute(
-            '''
-            INSERT INTO user_account (username, password_hash) VALUES 
-            (?, ?)
-            ''',
-            ('psadmin', hashlib.sha256(b'psadmin').digest())
-        )
-        conn.commit()
-    finally:
-        try:
-            cur.close()
-        except:
-            pass
+        '''
+    )
+    # Create admin user with default password.
+    conn.execute(
+        '''
+        INSERT INTO ps_user_account (username, password_hash) VALUES 
+        (?, ?)
+        ''',
+        ('psadmin', hashlib.sha256(b'psadmin').digest())
+    )
+    conn.commit()
 
 def create_directory_table(conn):
-    cur = conn.cursor()
-    try:
-        cur.execute(
-            '''
-            CREATE TABLE directory (
-                id INTEGER PRIMARY KEY NOT NULL,
-                name VARCHAR(256) NOT NULL
-            )
-            '''
+    logging.debug('Setting up ps_directory table')
+    conn.execute(
+        '''
+        CREATE TABLE ps_directory (
+            id INTEGER PRIMARY KEY NOT NULL,
+            name VARCHAR(256) NOT NULL,
+            is_hidden BOOLEAN NOT NULL DEFAULT 0
         )
-        # Create root directory.
-        cur.execute("INSERT INTO directory (name) VALUES ('/')")
-        cur.execute(
-            '''
-            CREATE TABLE link (
-                parent_id INTEGER NOT NULL,
-                child_id INTEGER NOT NULL,
-                PRIMARY KEY (parent_id, child_id),
-                FOREIGN KEY (parent_id) REFERENCES directory (id) ON DELETE CASCADE,
-                FOREIGN KEY (child_id) REFERENCES directory (id) ON DELETE CASCADE
-            )
-            '''
+        '''
+    )
+    # Create root directory.
+    conn.execute("INSERT INTO ps_directory (name) VALUES ('/')")
+    conn.execute(
+        '''
+        CREATE TABLE ps_link (
+            parent_id INTEGER NOT NULL,
+            child_id INTEGER NOT NULL,
+            PRIMARY KEY (parent_id, child_id),
+            FOREIGN KEY (parent_id) REFERENCES ps_directory (id) ON DELETE CASCADE,
+            FOREIGN KEY (child_id) REFERENCES ps_directory (id) ON DELETE CASCADE
         )
-        conn.commit()
-    finally:
-        try:
-            cur.close()
-        except:
-            pass
+        '''
+    )
+    conn.commit()
 
 def create_file_table(conn):
-    pass
+    logging.debug('Setting up ps_file table')
+    conn.execute(
+        '''
+        CREATE TABLE ps_file (
+            id INTEGER PRIMARY KEY NOT NULL,
+            name VARCHAR(256) NOT NULL,
+            parent_id INTEGER NOT NULL,
+            is_hidden BOOLEAN NOT NULL DEFAULT 0,
+            FOREIGN KEY (parent_id) REFERENCES ps_directory (id) ON DELETE CASCADE
+        )
+        '''
+    )
+    conn.commit()
 
 def create_file_version_table(conn):
-    pass
+    logging.debug('Setting up ps_file_version table')
+    conn.execute(
+        '''
+        CREATE TABLE ps_file_version (
+            file_id INTEGER NOT NULL,
+            version INTEGER NOT NULL,
+            remote_id VARCHAR(36),
+            FOREIGN KEY (file_id) REFERENCES ps_file (id) ON DELETE CASCADE,
+            PRIMARY KEY (file_id, version)
+        )
+        '''
+    )
+    conn.commit()
