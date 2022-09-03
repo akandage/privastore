6fb9f5ead18cffa724f5805851e3294747b2001d
Aruth Kandage <aruthkandage@gmail.com>
2022-09-02 21:01:13 -0400

    user_dao factory method missing self argument

--- file-server/file_server/local/db/user_dao.py
+++ file-server/file_server/local/db/user_dao.py
@@ -5,5 +5,5 @@ class UserDAO(DataAccessObject):
     def __init__(self, conn):
         super().__init__(conn)
     
-    def login_user(username, password):
+    def login_user(self, username, password):
         raise Exception('Not implemented!')
