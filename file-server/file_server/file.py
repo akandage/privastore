c3f786ecf1f29085f2f58bf34db281004667f925
Aruth Kandage <aruthkandage@gmail.com>
2022-10-03 13:13:32 -0400

    add file tests

--- file-server/file_server/file.py
+++ file-server/file_server/file.py
@@ -2,15 +2,17 @@ from curses import meta
 import os
 import uuid
 from .error import FileError
+from .file_chunk import default_chunk_encoder, default_chunk_decoder
 
 METADATA_FILE = '.metadata'
 
 class File(object):
 
-    def __init__(self, path, file_id=None, mode='r', encode_chunk=None, decode_chunk=None):
+    def __init__(self, path, file_id=None, mode='r', encode_chunk=default_chunk_encoder, decode_chunk=default_chunk_decoder):
         self._file_id = file_id or self.generate_file_id()
         self._file_path = os.path.join(path, self._file_id)
         self._mode = mode
+        self._closed = False
         self._chunks_read = 0
         self._chunks_written = 0
         self._total_chunks = 0
@@ -38,33 +40,49 @@ class File(object):
     def metadata_file_path(self):
         return os.path.join(self._file_path, METADATA_FILE)
     
+    def file_id(self):
+        return self._file_id
+    
+    def file_size(self):
+        return self._file_size
+    
+    def size_on_disk(self):
+        return self._size_on_disk
+
     def append_chunk(self, chunk_bytes):
+        if self._closed:
+            raise FileError('File closed')
         if self._mode != 'w':
             raise FileError('File not opened for writing')
         file_path = os.path.join(self._file_path, str(self._total_chunks+1))
         if os.path.exists(file_path):
             raise FileError('File chunk exists')
-        with open(file_path, 'w') as chunk_file:
+        with open(file_path, 'wb') as chunk_file:
             self._size_on_disk += self._encode_chunk(chunk_bytes, chunk_file)
         self._chunks_written += 1
         self._file_size += len(chunk_bytes)
         self._total_chunks += 1
     
     def read_chunk(self):
+        if self._closed:
+            raise FileError('File closed')
         if self._mode != 'r':
             raise FileError('File not opened for reading')
         if self._chunks_read < self._total_chunks:
             file_path = os.path.join(self._file_path, str(self._chunks_read+1))
             if not os.path.exists(file_path):
                 raise FileError('File chunk not found')
-            with open(file_path, 'w') as chunk_file:
-                chunk_bytes = self._decode_chunk(chunk_file)
+            with open(file_path, 'rb') as chunk_file:
+                chunk_bytes = self._decode_chunk(chunk_file=chunk_file)
             self._chunks_read += 1
             return chunk_bytes
 
     def close(self):
-        with open(METADATA_FILE, 'wb') as metadata_file:
-            metadata_file.write(self._total_chunks.to_bytes(4, 'big', signed=False))
-            metadata_file.write(self._file_size.to_bytes(4, 'big', signed=False))
-            metadata_file.write(self._size_on_disk.to_bytes(4, 'big', signed=False))
-            metadata_file.flush()
+        if not self._closed and self._mode == 'w':
+            metadata_file_path = self.metadata_file_path()
+            with open(metadata_file_path, 'wb') as metadata_file:
+                metadata_file.write(self._total_chunks.to_bytes(4, 'big', signed=False))
+                metadata_file.write(self._file_size.to_bytes(4, 'big', signed=False))
+                metadata_file.write(self._size_on_disk.to_bytes(4, 'big', signed=False))
+                metadata_file.flush()
+        self._closed = True
---
+++ file-server/file_server/test_file.py
@@ -0,0 +1,100 @@
+import os
+import random
+import shutil
+import unittest
+from .error import FileError
+from .file import File
+from .file_chunk import get_encrypted_chunk_encoder, get_encrypted_chunk_decoder
+from .util.crypto import get_encryptor_factory, get_decryptor_factory
+
+class TestFile(unittest.TestCase):
+    
+    def setUp(self):
+        os.mkdir('test_file')
+
+    def tearDown(self):
+        try:
+            shutil.rmtree('test_file')
+        except:
+            pass
+    
+    def test_write_file(self):
+        chunk1 = random.randbytes(1024)
+        chunk2 = random.randbytes(1024)
+        chunk3 = random.randbytes(100)
+
+        f = File('test_file', mode='w')
+        f.append_chunk(chunk1)
+        f.close()
+
+        self.assertEqual(len(chunk1), f.file_size())
+        self.assertTrue(f.size_on_disk() >= len(chunk1))
+
+        try:
+            f.append_chunk(chunk2)
+            self.fail('Expected file closed error')
+        except FileError as e:
+            self.assertEqual(str(e), 'File closed')
+        
+        try:
+            File('test_file', file_id=f.file_id(), mode='w')
+            self.fail('Expected error')
+        except:
+            pass
+
+        f2 = File('test_file', mode='w')
+        f2.append_chunk(chunk1)
+        f2.append_chunk(chunk2)
+        f2.append_chunk(chunk3)
+        f2.close()
+
+        self.assertEqual(len(chunk1)+len(chunk2)+len(chunk3), f2.file_size())
+        self.assertTrue(f2.size_on_disk() >= len(chunk1)+len(chunk2)+len(chunk3))
+        
+    def test_read_write_file(self):
+        chunk1 = random.randbytes(1024)
+        chunk2 = random.randbytes(1024)
+        chunk3 = random.randbytes(100)
+
+        f = File('test_file', mode='w')
+        f.append_chunk(chunk1)
+        f.append_chunk(chunk2)
+        f.append_chunk(chunk3)
+        f.close()
+
+        file_id = f.file_id()
+        file_size = f.file_size()
+        size_on_disk = f.size_on_disk()
+
+        f2 = File('test_file', file_id=file_id, mode='r')
+        self.assertEqual(f2.file_size(), file_size)
+        self.assertEqual(f2.size_on_disk(), size_on_disk)
+        self.assertEqual(chunk1, f2.read_chunk())
+        self.assertEqual(chunk2, f2.read_chunk())
+        self.assertEqual(chunk3, f2.read_chunk())
+        self.assertTrue(f2.read_chunk() is None)
+        f2.close()
+    
+    def test_read_write_encrypted_file(self):
+        key = os.urandom(16)
+        enc_factory = get_encryptor_factory('aes-128-cbc', key)
+        dec_factory = get_decryptor_factory('aes-128-cbc', key)
+        chunk_enc = get_encrypted_chunk_encoder(enc_factory)
+        chunk_dec = get_encrypted_chunk_decoder(dec_factory)
+
+        chunk1 = random.randbytes(1024)
+        chunk2 = random.randbytes(1024)
+        chunk3 = random.randbytes(100)
+
+        f = File('test_file', mode='w', encode_chunk=chunk_enc, decode_chunk=chunk_dec)
+        f.append_chunk(chunk1)
+        f.append_chunk(chunk2)
+        f.append_chunk(chunk3)
+        f.close()
+
+        f2 = File('test_file', file_id=f.file_id(), mode='r', encode_chunk=chunk_enc, decode_chunk=chunk_dec)
+        self.assertEqual(f2.read_chunk(), chunk1)
+        self.assertEqual(f2.read_chunk(), chunk2)
+        self.assertEqual(f2.read_chunk(), chunk3)
+        self.assertTrue(f2.read_chunk() is None)
+        f2.close()
