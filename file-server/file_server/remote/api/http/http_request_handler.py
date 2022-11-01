75b70000e1d7f8f402dc2d1da79ebfd39ff08808
Aruth Kandage <aruthkandage@gmail.com>
2022-11-01 17:34:44 -0400

    create remote package

---
+++ file-server/file_server/remote/api/http/http_request_handler.py
@@ -0,0 +1,13 @@
+from ....error import AuthenticationError, DirectoryError, FileError, SessionError
+from http import HTTPStatus
+from ....api.http.http_request_handler import BaseHttpApiRequestHandler
+from ....api.http.http_request_handler import CONNECTION_HEADER, CONNECTION_CLOSE, CONTENT_TYPE_HEADER, CONTENT_TYPE_JSON, CONTENT_LENGTH_HEADER
+import json
+import logging
+import urllib.parse
+
+class HttpApiRequestHandler(BaseHttpApiRequestHandler):
+
+    def __init__(self, request, client_address, server, controller):
+        self._controller = controller
+        super().__init__(request, client_address, server)
